7b65e4780323b011db5c8a1a1abdca88d0f71e2a
Jorgez-tech <jzuta309@gmail.com>
2025-09-28 18:02:32 -0300

    Limpieza de Home: secciones feature_section, client_section e info_section eliminadas o comentadas para vista principal. Configuración de rutas en App.js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,128 +1,25 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+import Nosotros from "./components/nosotros";
+import Jugadores from "./components/jugadores";
+import Contacto from "./components/contacto";
+
+
 function App() {
     return (
-        <div className="hero_area">
-            {/* Header Section */}
-            <header className="header_section">
-                <div className="container-fluid">
-                    <nav className="navbar navbar-expand-lg custom_nav-container">
-                        <a className="navbar-brand" href="index.html">
-                            <span>Sportpar</span>
-                        </a>
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <div className="d-flex ml-auto flex-column flex-lg-row align-items-center">
-                                <ul className="navbar-nav">
-                                    <li className="nav-item active">
-                                        <a className="nav-link" href="index.html">
-                                            Home <span className="sr-only">(current)</span>
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="about.html">About</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="product.html">Products</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="contact.html">Contact us</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Login</a>
-                                    </li>
-                                </ul>
-                                <div className="quote_btn-container">
-                                    <a href="" className="cart_link">
-                                        <i className="fa fa-cart-arrow-down" aria-hidden="true"></i>
-                                        <span className="cart_number">0</span>
-                                    </a>
-                                    <form className="form-inline">
-                                        <button className="btn nav_search-btn" type="submit">
-                                            <i className="fa fa-search" aria-hidden="true"></i>
-                                        </button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </header>
-            {/* End Header Section */}
-
-            {/* Slider Section */}
-            <section className="slider_section position-relative">
-                <ol className="carousel-indicators indicator-2">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active indicator-li-1">
-                        01
-                    </li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1">
-                        02
-                    </li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2">
-                        03
-                    </li>
-                </ol>
-                <div className="container-fluid h-100">
-                    <div className="row">
-                        <div className="col-md-5 col-xl-4 offset-xl-1">
-                            <div className="detail-box">
-                                <h1>
-                                    Sports and <br /> Fitness Store
-                                </h1>
-                                <p>There are many variations of passages of Lorem Ipsum available.</p>
-                                <div className="btn-box">
-                                    <a href="" className="btn-1">
-                                        Shop Now
-                                    </a>
-                                    <a href="" className="btn-2">
-                                        Get A Quote
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-7 px-0 h-100">
-                            <div className="img_container h-100">
-                                <div id="carouselExampleIndicators" className="carousel slide carousel-fade" data-ride="carousel">
-                                    <ol className="carousel-indicators indicator-1">
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active indicator-li-1"></li>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                                    </ol>
-                                    <div className="carousel-inner">
-                                        <div className="carousel-item active">
-                                            <div className="img-box">
-                                                <img src="images/slider-img.jpg" alt="" />
-                                            </div>
-                                        </div>
-                                        <div className="carousel-item">
-                                            <div className="img-box">
-                                                <img src="images/slider-img2.jpg" alt="" />
-                                            </div>
-                                        </div>
-                                        <div className="carousel-item">
-                                            <div className="img-box">
-                                                <img src="images/slider-img3.jpg" alt="" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/* End Slider Section */}
-        </div>
+        <Router>
+            <div className="hero_area">
+                {/* Header Section - se recomienda migrar a componente si se desea reutilizar */}
+                {/* Aquí podrías importar y usar un componente Header si lo tienes */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/nosotros" element={<Nosotros />} />
+                    <Route path="/jugadores" element={<Jugadores />} />
+                    <Route path="/contacto" element={<Contacto />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,267 @@
+
+export default function Home() {
+  // This component is a JSX conversion of the original home HTML page.
+  // It omits <html>, <head>, <body> and external <link>/<meta> tags.
+  // Keep meta/link tags in public/index.html or the app root.
+
+  const handleSubmitNoop = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div className="hero_area">
+        {/* header section starts */}
+        <header className="header_section">
+          <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg custom_nav-container ">
+              <a className="navbar-brand" href="/">
+                <span>Sportpar</span>
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="d-flex ml-auto flex-column flex-lg-row align-items-center">
+                  <ul className="navbar-nav">
+                    <li className="nav-item active">
+                      <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/about">About</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/products">Products</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/contact">Contact us</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Login</a>
+                    </li>
+                  </ul>
+                  <div className="quote_btn-container">
+                    <a href="#" className="cart_link">
+                      <i className="fa fa-cart-arrow-down" aria-hidden="true" />
+                      <span className="cart_number">0</span>
+                    </a>
+                    <form className="form-inline" onSubmit={handleSubmitNoop}>
+                      <button className="btn nav_search-btn" type="submit">
+                        <i className="fa fa-search" aria-hidden="true" />
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </header>
+        {/* end header section */}
+
+        {/* slider section */}
+        <section className="slider_section position-relative">
+          <ol className="carousel-indicators indicator-2">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active indicator-li-1">01</li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1">02</li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2">03</li>
+          </ol>
+          <div className="container-fluid h-100">
+            <div className="row">
+              <div className="col-md-5 col-xl-4 offset-xl-1">
+                <div className="detail-box">
+                  <h1>
+                    Sports and <br />
+                    Fitness Store
+                  </h1>
+                  <p>There are many variations of passages of Lorem Ipsum available.</p>
+                  <div className="btn-box">
+                    <a href="#" className="btn-1">Shop Now</a>
+                    <a href="#" className="btn-2">Get A Quote</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-7 px-0 h-100">
+                <div className="img_container h-100">
+                  <div id="carouselExampleIndicators" className="carousel slide carousel-fade" data-ride="carousel">
+                    <ol className="carousel-indicators indicator-1">
+                      <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active indicator-li-1" />
+                      <li data-target="#carouselExampleIndicators" data-slide-to="1" />
+                      <li data-target="#carouselExampleIndicators" data-slide-to="2" />
+                    </ol>
+                    <div className="carousel-inner">
+                      <div className="carousel-item active">
+                        <div className="img-box">
+                          <img src="/images/slider-img.jpg" alt="slide 1" />
+                        </div>
+                      </div>
+                      <div className="carousel-item">
+                        <div className="img-box">
+                          <img src="/images/slider-img2.jpg" alt="slide 2" />
+                        </div>
+                      </div>
+                      <div className="carousel-item">
+                        <div className="img-box">
+                          <img src="/images/slider-img3.jpg" alt="slide 3" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+
+      {/*
+      <section className="about_section">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-4 col-md-5 offset-md-1">
+              <div className="detail-box">
+                <h2>About Us</h2>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus hic, dolore error nulla similique cum minima rem, distinctio animi architecto inventore temporibus consequuntur quibusdam modi atque est fuga dicta quae!
+                </p>
+                <div>
+                  <a href="#">Buy Now</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-7 col-md-5">
+              <div className="img-box">
+                <img src="/images/about-img.jpg" alt="about" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      */}
+
+      {/*
+      <section className="product_section layout_padding">
+        <div className="container">
+          <div className="heading_container heading_center">
+            <h2>Our Products</h2>
+          </div>
+          <div className="row">
+            {Array.from({ length: 6 }).map((_, i) => (
+              <div className="col-sm-6 col-lg-4" key={i}>
+                <div className="box">
+                  <div className="img-box">
+                    <img src={`/images/p${(i % 9) + 1}.png`} alt={`product ${i + 1}`} />
+                  </div>
+                  <div className="detail-box">
+                    <div className="text">
+                      <a href="#" className="p_name">Sport Product</a>
+                      <h5><span>$</span> 300</h5>
+                    </div>
+                    <div className="like">
+                      <h6>Rating</h6>
+                      <div className="star_container">
+                        <i className="fa fa-star" aria-hidden="true" />
+                        <i className="fa fa-star" aria-hidden="true" />
+                        <i className="fa fa-star" aria-hidden="true" />
+                        <i className="fa fa-star" aria-hidden="true" />
+                        <i className="fa fa-star" aria-hidden="true" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      */}
+
+      {/*
+      <section className="contact_section layout_padding">
+        <div className="container">
+          <div className="heading_container heading_center">
+            <h2>Get In Touch</h2>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <form onSubmit={handleSubmitNoop}>
+                <div className="form-row">
+                  <div className="form-group col-md-6">
+                    <input type="text" className="form-control" placeholder="Your Name" />
+                  </div>
+                  <div className="form-group col-md-6">
+                    <input type="email" className="form-control" placeholder="Email" />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <input type="text" className="form-control" placeholder="Phone Number" />
+                </div>
+                <div className="form-group">
+                  <textarea className="form-control" placeholder="Message" rows="5" />
+                </div>
+                <button type="submit" className="btn btn-primary">Send</button>
+              </form>
+            </div>
+            <div className="col-md-6">
+              <div className="map_container">
+                <img src="/images/contact_bg.jpg" alt="map" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      */}
+
+      {/*
+      <section className="client_section layout_margin-bottom">
+        <div className="container">
+          <div className="heading_container heading_center">
+            <h2>Our Clients</h2>
+          </div>
+          <div className="client_container">
+            <div className="client_logo"><img src="/images/client.jpg" alt="client" /></div>
+            <div className="client_logo"><img src="/images/client.jpg" alt="client" /></div>
+            <div className="client_logo"><img src="/images/client.jpg" alt="client" /></div>
+          </div>
+        </div>
+      </section>
+      */}
+
+      {/*
+      <section className="info_section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <h5>About</h5>
+              <p>Short about text or address goes here.</p>
+            </div>
+            <div className="col-md-4">
+              <h5>Contact</h5>
+              <p>Phone: +123456789</p>
+            </div>
+            <div className="col-md-4">
+              <h5>Hours</h5>
+              <p>Mon - Fri: 9am - 6pm</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      */}
+
+      <footer className="footer_section">
+        <div className="container">
+          <p>© 2025 Sportpar - All Rights Reserved</p>
+        </div>
+      </footer>
+    </>
+  );
+}
